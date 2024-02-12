81eaf74f1a1655fd9bce9d810acfc12f784d21fa
newduwns001210 <newduwns001210@naver.com>
2024-02-12 22:22:39 +0900

    plus

--- redux/src/index.js
+++ redux/src/index.js
@@ -1,11 +1,16 @@
 import { legacy_createStore } from "redux";
 
+const add = document.getElementById("add");
+const minus = document.getElementById("minus");
+const number = document.querySelector("span");
+
 const countReducer = (count = 0, action) => {
   // state의 값을 설정해주지 않으면 default 값으로 0이 설정됨.
-  if (action.type === "Add") {
-    count++;
-  } else if (action.type === "Minus") {
-    count--;
+  console.log(count, action);
+  if (action.type === "ADD") {
+    return count + 1;
+  } else if (action.type === "MINUS") {
+    return count - 1;
   } else {
     return count;
   }
@@ -13,10 +18,20 @@ const countReducer = (count = 0, action) => {
 
 const countStore = legacy_createStore(countReducer);
 
+const handleAdd = () => {
+  countStore.dispatch({ type: "ADD" });
+};
+
+const handleMinus = () => {
+  countStore.dispatch({ type: "MINUS" });
+};
+
+add.addEventListener("click", handleAdd);
+minus.addEventListener("click", handleMinus);
+/*
 countStore.dispatch({ type: "Add" });
 countStore.dispatch({ type: "Add" });
 countStore.dispatch({ type: "Add" });
 countStore.dispatch({ type: "Minus" });
 // dispatch로 Add, Minus object를 보내 해당 action 값을 return 받음.
-
-console.log(countStore.getState());
+*/
